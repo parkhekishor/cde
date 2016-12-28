3db779d5b91ee59bf9835a9642ec1b404e58ec0e
pacharya <piyush.r.acharya@gmail.com>
2016-12-28 16:59:06 +0530

    new structure

---
+++ cde/CDEDomain/src/main/java/org/cde/cdedomain/App.java
@@ -0,0 +1,13 @@
+package org.cde.cdedomain;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
