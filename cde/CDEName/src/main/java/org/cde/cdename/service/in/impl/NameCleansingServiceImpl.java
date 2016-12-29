b11755fcf1614727bd2a53cfa7adff689f723ab3
pacharya <piyush.r.acharya@gmail.com>
2016-12-29 16:39:21 +0530

    Modification in name service clease

--- cde/CDEName/src/main/java/org/cde/cdename/service/in/impl/NameCleansingServiceImpl.java
+++ cde/CDEName/src/main/java/org/cde/cdename/service/in/impl/NameCleansingServiceImpl.java
@@ -3,7 +3,7 @@ package org.cde.cdename.service.in.impl;
 import org.cde.cdename.service.NameCleansingService;
 
 public class NameCleansingServiceImpl implements NameCleansingService{
-	public static String cleanse(String rec)
+	public  String cleanse(String rec)
 	{
 		return null;
 	}
